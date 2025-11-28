b4e2172f80fab5e71c0dea6f9fe9e1b1d292cb50
良子 <2024308119@qq.com>
2025-11-29 04:01:53 +0800

    Revert "low 01"
    This reverts commit 4833df7023623769df4fa285239f5d23a864cb34.

--- api/app/Http/Middleware/EnableCrossRequestMiddleware.php
+++ api/app/Http/Middleware/EnableCrossRequestMiddleware.php
@@ -24,6 +24,15 @@ class EnableCrossRequestMiddleware
      */
     public function handle($request, Closure $next)
     {
-        return $next($request);
+        $origin = $request->server('HTTP_ORIGIN') ? $request->server('HTTP_ORIGIN') : '';
+        $resp = $next($request);
+        $resp->headers->add([
+            'Access-Control-Allow-Origin' => $origin,
+            'Access-Control-Allow-Headers' => 'Origin, Content-Type, Cookie, X-CSRF-TOKEN, Accept, Authorization,applyid,openid,apply-secret,versionid, X-XSRF-TOKEN,Lang',
+            'Access-Control-Expose-Headers' => 'Authorization, authenticated',
+            'Access-Control-Allow-Methods' => 'GET, POST, PATCH, PUT, OPTIONS,DELETE',
+            'Access-Control-Allow-Credentials' => 'true'
+        ]);
+        return $resp;
     }
 }
