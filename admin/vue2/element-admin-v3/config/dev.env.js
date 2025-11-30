a15d9998587fc247fe63a63c086ae6854b7ca206
良子 <2024308119@qq.com>
2025-11-30 23:42:28 +0800

    init

--- admin/vue2/element-admin-v3/config/dev.env.js
+++ admin/vue2/element-admin-v3/config/dev.env.js
@@ -2,7 +2,7 @@ module.exports = {
   NODE_ENV: '"development"',
   ENV_CONFIG: '"dev"',
   SITE_NAME: '"TFSHOP电商商城"',
-  BASE_API: '"http://tfshop.test/api/v1/admin/"',
-  H5_URL: '"http://tfshop.test/h5/#/"',
-  BASE_HTTP: '"http://tfshop.test"'
+  BASE_API: '"http://tfshop.testk3/api/v1/admin/"',
+  H5_URL: '"http://tfshop.testk3/h5/#/"',
+  BASE_HTTP: '"http://tfshop.testk3"'
 }
